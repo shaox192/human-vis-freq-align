8ff5abcd67be762f63023215902a4fdfd772166c
shaox192 <70292345+shaox192@users.noreply.github.com>
2024-11-26 23:33:05 -0600

    cleaned up

--- attack_alg/adv.py
+++ attack_alg/adv.py
@@ -38,24 +38,15 @@ def foolbox_attack(val_loader,
     for i, (images, target) in enumerate(val_loader):
         images = images.to(device)
         target = target.to(device)
-
-        output = fmodel(images)
-        output = fmodel(images).argmax(axis=-1)
         clean_acc = accuracy(fmodel, images, target)
 
         n += len(images)
         original_acc_sum += clean_acc * len(images)
-        # print(f"[{i + 1}]/[{len(val_loader)}]")
-        # print(f"clean accuracy:  {clean_acc * 100:.2f}, avg so far: {original_acc_sum / n * 100:.2f}", flush=True)
-        # try:
+
         raw_advs, clipped_advs, success = attack(fmodel, images, target, epsilons=[epsilon])
-        # print("------ finished attack -------", flush=True)
 
         robust_accuracy = 1 - success.float().mean(axis=-1)
-        # print("robust accuracy for perturbations with", end=': ')
-        # for i, (eps, acc) in enumerate(zip(epsilons, robust_accuracy)):
         perturb_acc += robust_accuracy[0].item() * len(images)
-        # print(f"Linf norm ≤ {epsilons[0]:.4f}: {acc.item() * 100:.2f}, avg so far: {perturb_accs[i] / n * 100:.2f}", flush=True)
     
     return original_acc_sum, perturb_acc, n
 
