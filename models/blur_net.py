54cba242514082b7e4da0453615ef49fb6407c46
shaox192 <70292345+shaox192@users.noreply.github.com>
2024-11-26 23:41:08 -0600

    blur_net skeleton, needs filling in

--- models/blur_net.py
+++ models/blur_net.py
@@ -1,56 +1,48 @@
-from typing import Optional
-
 import torch
 import torch.nn as nn
 # from jarvis.models import ImageClassifier, ResNet
 
-Tensor = torch.Tensor
+# Tensor = torch.Tensor
 
 class BlurNet(nn.Module):
-    r"""BlurNet model.
-
-    A ResNet model with a fixed blurring preprocessing layer.
-
-    """
-
-    def __init__(self,
-        resnet,
-        sigma: Optional[float] = 1.5,
-    ):
-        r"""
-        Args
-        ----
-        resnet:
-            A ResNet model.
-        sigma:
-            Gaussian blurring parameter.
-
+    def __init__(self, classifier, kernel_size, sigma):
+        """
+        :param classifier:
         """
         super(BlurNet, self).__init__()
-        self.resnet = resnet
-        self.in_channels = self.resnet.in_channels
-        self.num_classes = self.resnet.num_classes
-        self.normalizer = self.resnet.normalizer
+        self.classifier = classifier
+        in_channels = self.classifier.conv1.in_channels
 
         self.sigma = sigma
-        if sigma is None:
-            self.blur = nn.Sequential()
-        else:
-            half_size = int(-(-2.5*sigma//1))
-            x, y = torch.meshgrid(
-                torch.arange(-half_size, half_size+1).to(torch.float),
-                torch.arange(-half_size, half_size+1).to(torch.float),
-                )
-            w = torch.exp(-(x**2+y**2)/(2*sigma**2))
-            w /= w.sum()
-            self.blur = nn.Conv2d(
-                self.in_channels, self.in_channels, 2*half_size+1,
-                padding=half_size, padding_mode='circular', bias=False,
-            )
-            self.blur.weight.data *= 0
-            for i in range(self.in_channels):
-                self.blur.weight.data[i, i] = w
-            self.blur.weight.requires_grad = False
+        self.blur = None # nn.Conv2d(in_channels, in_channels, kernel_size, padding='same', padding_mode='reflect', bias=False)
+        self._init_filter(kernel_size, in_channels, sigma)
 
-    def layer_activations(self, x: Tensor) -> tuple[list[Tensor], list[Tensor], Tensor]:
-        return self.resnet.layer_activations(self.blur(x))
+    def _init_filter(self, kernel_size, num_channel, sigma):
+        
+        #TODO: Implement blur layer
+        ####: code from https://github.com/lizhe07/blur-net/blob/master/blurnet/models.py
+        # half_size = int(-(-2.5*sigma//1))
+        # x, y = torch.meshgrid(
+        #     torch.arange(-half_size, half_size+1).to(torch.float),
+        #     torch.arange(-half_size, half_size+1).to(torch.float),
+        #     )
+        # w = torch.exp(-(x**2+y**2)/(2*sigma**2))
+        # w /= w.sum()
+        # self.blur = nn.Conv2d(
+        #     self.in_channels, self.in_channels, 2*half_size+1,
+        #     padding=half_size, padding_mode='circular', bias=False,
+        # )
+        # self.blur.weight.data *= 0
+        # for i in range(self.in_channels):
+        #     self.blur.weight.data[i, i] = w
+        # self.blur.weight.requires_grad = False
+        return
+    
+    def forward(self, x, filter_only=False):
+       
+        x = self.blur(x)
+
+        if filter_only:
+            return x
+        x = self.classifier(x)
+        return x
