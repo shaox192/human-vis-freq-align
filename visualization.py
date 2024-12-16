551e2d14a8d7fb62b5794a71029bdb67daf979a0
Tianyu Ren <tianyur2@illinois.edu>
2024-12-16 15:51:35 -0600

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -12,15 +12,13 @@ from models import BandPassNet, get_classifier, BlurNet
 # ================================
 def load_model():
     # 配置参数
-    num_classes = 50  # 确保与训练时一致
+    num_classes = 50
     kernel_size = 31
     custom_sigma = 1.5
     custom_sigma = 0.424619
 
-    # 加载基础 ResNet 模型
     classifier = get_classifier("resnet18", num_classes, pretrained=True)
 
-    # 包装成 BandPassNet
     model = BandPassNet(classifier, kernel_size, custom_sigma)
     # model = BlurNet(classifier, custom_sigma)
     model = classifier
@@ -41,16 +39,16 @@ def hook_fn(name):
 
 def register_hooks(model):
     for name, module in model.named_modules():
-        if isinstance(module, nn.Conv2d):  # 只注册卷积层
+        if isinstance(module, nn.Conv2d):
             if name == "conv_real":
                 module.register_forward_hook(hook_fn(name))
-        # if name == "classifier.layer4":  # ResNet 的最后一个卷积模块
+        # if name == "classifier.layer4":
         #     module.register_forward_hook(hook_fn("layer4"))
-        # elif name == "classifier.avgpool":  # 全局平均池化之前
+        # elif name == "classifier.avgpool":
         #     module.register_forward_hook(hook_fn("avgpool"))
 
 def get_transform():
-    return transforms.Compose([  # 调整为与训练一致的尺寸
+    return transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
@@ -59,14 +57,14 @@ def get_transform():
 
 def load_image(image_path, transform):
     image = Image.open(image_path).convert("RGB")
-    input_tensor = transform(image).unsqueeze(0)  # 增加 batch 维度
+    input_tensor = transform(image).unsqueeze(0)
     return input_tensor
 
 def visualize_activations():
     channels = 8
     for layer_name, feature_map in activation.items():
-        num_channels = feature_map.shape[1]  # 通道数
-        fig, axs = plt.subplots(1, min(num_channels, channels), figsize=(20, 5))  # 每层显示前 8 个通道
+        num_channels = feature_map.shape[1]
+        fig, axs = plt.subplots(1, min(num_channels, channels), figsize=(20, 5))
         fig.suptitle(f"Layer: {layer_name}")
 
         for i in range(min(num_channels, channels)):
@@ -80,14 +78,12 @@ if __name__ == "__main__":
 
     register_hooks(model)
 
-    image_path = "n02123159_667.JPEG"  # 替换为实际图片路径
+    image_path = "n02123159_667.JPEG"
     transform = get_transform()
     input_tensor = load_image(image_path, transform)
 
-    # 前向传播
     with torch.no_grad():
         output = model(input_tensor)
     print(torch.argmax(output, dim=1))
 
-    # 可视化特征图
     visualize_activations()
